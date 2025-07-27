7dc9c9d5800f21dfeb591f389d01fafa10977bc1
Shvetank Dattatreya <hello.shvetank@gmail.com>
2025-07-27 09:56:17 +0530

    Added odd even checker and updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-projects
 A repo of small and simple python projects.
+
+I hope to add various simple programs to this repo.
---
+++ even-odd-checker/odd-even.py
@@ -0,0 +1,8 @@
+print("Welcome to this Odd/Even checker")
+input_number = input("Enter a number: ")
+number = int(input_number)
+
+if number % 2 == 0:
+    print("This is an Even number.")
+else:
+    print("This is an Odd number.")
